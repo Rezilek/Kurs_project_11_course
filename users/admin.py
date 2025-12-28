33dbc8a9de47d2029bf92711da5c5924ff239f66
Rezilek <rezilek@gmail.com>
2025-12-28 19:42:15 +0300

    Initial commit: Django DRF project with Courses, Lessons, Users

---
+++ users/admin.py
@@ -0,0 +1,28 @@
+# users/admin.py
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = User
+    ordering = ('email',)
+    list_display = ('email', 'is_staff', 'is_active',)
+    list_filter = ('email', 'is_staff', 'is_active',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'groups', 'user_permissions')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'phone', 'city', 'avatar')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active')}
+        ),
+    )
+    search_fields = ('email',)
+    filter_horizontal = ('groups', 'user_permissions',)
+
+admin.site.register(User, CustomUserAdmin)
